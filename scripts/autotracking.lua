74b2af53550d8b2e2b73a70343486ea38cabbbb4
lurch9229 <lukey.young92@gmail.com>
2026-08-11 23:20:22 +0100

    Finalised Autotracking
    added correct format for consumables in mapping
    Fixed Multiplier null error in archipelago.lua
    Fixed missing extension in call for archipelago.lua (BIGGEST CULPRIT)
    moved boss trophies to hologram check as there does not appear to be check for defeating bosses themselves
    Fixed code for simple ammo

--- scripts/autotracking.lua
+++ scripts/autotracking.lua
@@ -1,17 +1,15 @@
--- Settings -------------------------------------------
-AUTOTRACKER_ENABLE_ITEMS_TRACKING = true
-AUTOTRACKER_ENABLE_LOCATIONS_TRACKING = true
-
 -- Configuration --------------------------------------
-AUTOTRACKER_ENABLE_DEBUG_LOGGING = false
-AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP = false
+AUTOTRACKER_ENABLE_ITEM_TRACKING = true
+AUTOTRACKER_ENABLE_LOCATION_TRACKING = true and not IS_ITEMS_ONLY
+AUTOTRACKER_ENABLE_DEBUG_LOGGING = false and ENABLE_DEBUG_LOG
+AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP = true and AUTOTRACKER_ENABLE_DEBUG_LOGGING
 AUTOTRACKER_ENABLE_DEBUG_LOGGING_SNES = true and AUTOTRACKER_ENABLE_DEBUG_LOGGING
 -------------------------------------------------------
 print("")
 print("Active Auto-Tracker Configuration")
 print("---------------------------------------------------------------------")
-print("Enable Item Tracking:        ", AUTOTRACKER_ENABLE_ITEMS_TRACKING)
-print("Enable Location Tracking:    ", AUTOTRACKER_ENABLE_LOCATIONS_TRACKING)
+print("Enable Item Tracking:        ", AUTOTRACKER_ENABLE_ITEM_TRACKING)
+print("Enable Location Tracking:    ", AUTOTRACKER_ENABLE_LOCATION_TRACKING)
 if AUTOTRACKER_ENABLE_DEBUG_LOGGING then
     print("Enable Debug Logging:        ", AUTOTRACKER_ENABLE_DEBUG_LOGGING)
     print("Enable AP Debug Logging:        ", AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP)
@@ -19,5 +17,11 @@ if AUTOTRACKER_ENABLE_DEBUG_LOGGING then
 end
 print("---------------------------------------------------------------------")
 print("")
+
+-- uncomment this to use `Tracker.AllowDeferredLogicUpdate` to defer logic updates, which might be needed if you touch A LOT of items/locations at once.
+-- if Tracker.AllowDeferredLogicUpdate == false then Tracker.AllowDeferredLogicUpdate = true end
+
 -- loads the AP autotracking code
-require("scripts/autotracking/archipelago")
+ScriptHost:LoadScript("scripts/autotracking/archipelago.lua")
+-- loads the SNES autotracking code
+ScriptHost:LoadScript("scripts/autotracking/snes.lua")
